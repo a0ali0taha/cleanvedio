47aa3bcd0592ebeb85c1e50aad7bbdfa8940bd68
Ahmed Ali <a.ali.taha@gmail.com>
2023-07-31 22:46:18 +0300

    playlist

--- main.py
+++ main.py
@@ -8,40 +8,42 @@ from spleeter import separate_vocals_with_spleeter
 from audio_merge import combine_audio_segments
 from attach import attach_audio
 def start(url,is_playlist = True, output_path='videos',is_send_to_telegram=False):
+    i=1
     if is_playlist:
         video_paths=download_playlist(url, output_path)
         for title, file_path in video_paths.items():
+                output_path=f"{output_path}\\{i}"
                 # Do something with the title and file_path
                 print(f"Video Title: {title}, File Path: {file_path}")
-                handle(title,file_path,is_send_to_telegram)
+                handle(title,output_path,file_path,is_send_to_telegram)#vtitile,newv\1,newv\vtitle.mp4
+                i=i+1
         
     else:
         video_title, video_path = download_video(url, output_path)
-        handle(video_title,video_path,is_send_to_telegram)
+        output_path=f"{output_path}\\{i}"
+        handle(video_title,output_path,video_path,is_send_to_telegram)
         
 
         
-def handle(video_name,video_path,is_send_to_telegram=False):
-    detached_audio_path= video_path.replace('mp4','mp3')
+def handle(video_name,output_path,video_path,is_send_to_telegram=False):
+    #video_name=vtitile,   output_path=newv\1,    video_path=newv\vtitle.mp4
+    detached_audio_path= video_path.replace('mp4','mp3')#newv\vtitle.mp3
     detach_audio(video_path,detached_audio_path)
     segments_files=split(detached_audio_path)
-    vocal_folder='output_folder'
+    vocals_folder=f'{output_path}\\vocals'
+    vocal_files = []
     for mp3_file in segments_files: 
-        separate_vocals_with_spleeter(mp3_file,vocal_folder)
-        # directory_path = '{output_folder}\\{vedoioname}_{1}\vocals.mp3'
+        vocal_folder_seg=f'{vocals_folder}\\{mp3_file}'
+        separate_vocals_with_spleeter(mp3_file,vocal_folder_seg)
+        vocal_files.append(f'{vocal_folder_seg}\\{video_name}\\vocals.mp3')
 
-    directory_path = 'output_folder'
-
-    # List all files in the directory
-    folder_list = os.listdir(directory_path)
-    segment_files=[]
-    for vocal_folder in folder_list:
-    # Perform operations on each MP3 file
-        segment_files.append(directory_path+'\\'+vocal_folder+'\\vocals.mp3')
-    combined_audio = combine_audio_segments(segment_files)
-    output_file = "output_folder\\combined_audio.mp3"
-    combined_audio.export(output_file, format="mp3")
-    final_video_path="final\\{output_path}\\{video_name}"
-    attach_audio(video_path, output_file, final_video_path)
+    combined_audio = combine_audio_segments(vocal_files)
+    combined_audio_file = f"{output_path}\\combined_audio.mp3"
+    combined_audio.export(combined_audio_file, format="mp3")
+    final_video_path=f"final\\{video_name}.mp4"
+    attach_audio(video_path, combined_audio_file, final_video_path)
     if is_send_to_telegram:
         asyncio.run(send_to_telegram(video_name, final_video_path))
+
+
+# handle('الحرب العالمية الأولى باختصار','newv\\1','newv\\الحرب العالمية الأولى باختصار.mp4',True)
